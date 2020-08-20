7cf011662f47e55a7eff64bc69e94b8f8a497a38
dezzh@mail.ru <1993dez1993>
2020-08-21 02:37:18 +0800

    lesson 2

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-console.log(typeof null +' this is bag');
+const firstRow = prompt('Введите первую строку');
+const secondRow = prompt('Введите вторую строку строку');
+let firstCount = 0;
+let secondCount = 0;
+function getRow(firstRow, secondRow) {
+    for(let i=0;i<firstRow.length;i++){
+        if(firstRow.charAt(i) === 'а'){
+            firstCount++;
+        }
+    }
+    for(let i=0;i<secondRow.length;i++){
+        if(secondRow.charAt(i) === 'а'){
+            secondCount++;
+        }
+    }
+    if(firstCount>secondCount){
+        return 'в первой строке больше - '+firstCount+', а во второй - '+secondCount;
+    }
+    return 'во второй строке больше - '+secondCount+', а в первой - '+firstCount;
+
+}
+
+alert(getRow(firstRow, secondRow));
+
